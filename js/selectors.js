05b4468f2df04f52d316eacc222502bf986cdaf3
Jonah Chates <jonah.chates@gmail.com>
2019-06-16 13:42:27 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,12 @@
 // declare your functions here...
+function paragraphSelector() {
+  return $('img');}
+function lastImageSelector() {
+  return $('img:last');}
+function ninjaBabySelector() {
+  return $('#ninja-baby');}
+function divSelector() {
+  return $('.pics');}
+function firstListItem() {
+  return $('#pic-list li).first();}
+
